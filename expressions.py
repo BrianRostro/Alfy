1cb11c8b222f151ad447ac039921750d8e9ad911
Brian Rostro <rostro.brian@yahoo.com>
2022-11-05 19:44:19 -0400

    Update expressions.py

--- expressions.py
+++ expressions.py
@@ -5,5 +5,13 @@ def calc_math_expression(num1, num2, operator):
     
     if operator == "+":
         print(num1 + num2)
+    elif operator == "-":
+        print(num1 - num2)
+    elif operator == "*":
+        print(num1 * num2)
+    elif operator == "/":
+        print(num1 / num2)
+    else:
+        print("This is an invalid operator.")
 
 calc_math_expression()
